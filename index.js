056138c09dd97408c2f9b042c10c4adf988ce3e8
aisuzuki <spiral@thefool.spiral.net>
2021-04-10 20:15:02 +0200

    use aws

--- index.js
+++ index.js
@@ -1,59 +1,51 @@
-const { Client, MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed, ReactionUserManager } = require('discord.js');
 const axios = require('axios');
 const fs = require('fs');
-const cld = require('cld');
 const aws = require('aws-sdk');
+const {
+  ComprehendClient
+} = require('@aws-sdk/client-comprehend-node/ComprehendClient');
+const {
+  BatchDetectDominantLanguageCommand
+} = require('@aws-sdk/client-comprehend-node/commands/BatchDetectDominantLanguageCommand');
 
 const DEFAULT_LANG = 'EN';
-const AUTH_FILE = './auth.json';
+const CONFIG = require('./auth.json');
+const POLLY_VALUES = require('./polly.json');
 
-const awsLanguageCode = [ //todo
-  'arb',
-  'cmn-CN',
-  'cy-GB',
-  'da-DK',
-  'de-DE',
-  'en-AU',
-  'en-GB',
-  'en-GB-WLS',
-  'en-IN',
-  'en-US',
-  'es-ES',
-  'es-MX',
-  'es-US',
-  'fr-CA',
-  'fr-FR',
-  'is-IS',
-  'it-IT',
-  'ja-JP',
-  'hi-IN',
-  'ko-KR',
-  'nb-NO',
-  'nl-NL',
-  'pl-PL',
-  'pt-BR',
-  'pt-PT',
-  'ro-RO',
-  'ru-RU',
-  'sv-SE',
-  'tr-TR',
-];
+if (!fs.existsSync('./userconf.json')) {
+  fs.writeFileSync('./userconf.json', JSON.stringify({}));
+}
+const USERCONF = require('./userconf.json');
 
-let token = '';
-let prefix = '!tts';
+let prefix = CONFIG.prefix;
 
 const streamMapping = new Map();
 const client = new Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.once('reconnecting', () => {
-	console.log('Reconnecting!');
+  console.log('Reconnecting!');
 });
 client.once('disconnect', () => {
-	console.log('Disconnect!');
+  console.log('Disconnect!');
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  if (oldState.channel === null) return;
+  if (oldState.channel.members.size === 1 && oldState.channel.members.find(m => m.displayName === 'i18n-text-to-speech') !== null) {
+    const channelName = oldState.channel.name;
+    const streaming = streamMapping.get(channelName);
+    if (!streaming) {
+      return;
+    }
+    const vc = streaming.vc;
+    await vc.disconnect();
+    streamMapping.delete(channelName);
+  }
 });
 
 client.on('message', async message => {
@@ -65,26 +57,59 @@ client.on('message', async message => {
 
   // print help
   if (message.content.startsWith(`${prefix} help`)) {
-      message.channel.send(
-        '!tts connect - connect vc/vcにbotが参加します\n' +
-        '!tts disconnect - disconnect vc/vcからbot退出します\n' +
-        '!tts help - you know what it is\n'
-      );
-      return;
+    message.channel.send(
+      '!tts connect - connect vc/vcにbotが参加します\n' +
+      '!tts disconnect - disconnect vc/vcからbot退出します\n' +
+      '!tts voice {language(e.g, en)/言語} {language code/言語コード} {voice id/言語ID} - change language accent/言語別の発音を設定します\n' +
+      '!tts la list - display language code list/言語コードのリストを表示します\n' +
+      '!tts vid list - display voice id list/音声IDの一覧を表示します\n' +
+      '!tts help - you know what it is\n'
+    );
+    return;
   }
 
+  if (message.content === '') return;
+  if (message.content.startsWith('http')) return;
   if (!message.guild) return;
   if (!message.member.voice.channel) return;
+  /*
+if (!message.member.voice.channel) {
+  message.reply('Join voice channel first/最初に Voice channel 参加してください');
+  return;
+}
+*/
   const channelName = message.member.voice.channel.name;
 
   if (message.content.startsWith(`${prefix} connect`)) {
+    if (streamMapping.get(channelName)) {
+      // TODO
+    }
     const vc = await message.member.voice.channel.join();
     if (!vc) {
       message.reply('Could not join the channel/vc に参加できませんでした');
       return;
     }
     message.reply(`Joinned ${channelName}/ ${channelName} に接続しました`);
-    streamMapping.set(channelName, { vc, datetime: Date.now(), });
+    streamMapping.set(channelName,
+      {
+        vc,
+        playing: false,
+        queue: [],
+        datetime: Date.now()
+      }
+    );
+
+    if (!USERCONF[message.author.username]) {
+      // todo
+      USERCONF[message.author.username] = {
+      };
+      try {
+        fs.writeFileSync('./userconf.json', JSON.stringify(USERCONF));
+      } catch (err) {
+        console.log('could not save user conf: ' + err);
+      }
+    }
+
     return;
   } else if (message.content.startsWith(`${prefix} disconnect`)) {
     const streaming = streamMapping.get(channelName);
@@ -96,51 +121,157 @@ client.on('message', async message => {
     await vc.disconnect();
     streamMapping.delete(channelName);
     return;
-  }
+  } else if (message.content.startsWith(`${prefix} voice `)) {
 
-  const streaming = streamMapping.get(channelName); 
-  if (!streaming) return; // not connected.
-
-  let langCode;
-  let voiceId;
-  try {
-    const detectedLangs = await cld.detect(message.content)
-    langCode = detectedLangs.languages[0].code;
-  } catch (error) {
-    message.reply('Failed to detect language/言語の検知に失敗しました');
-    console.log(error);
+      const values = message.content.slice(`${prefix} voice `.length).split(' ');
+      if (values.length !== 3) {
+        message.reply(
+          'please define language, language code and voice id/言語、言語コード、音声IDを正しく設定してください \n' +
+          'Example: \n' +
+          prefix + ' voice en en-GB Amy \n\n' +
+          '例: \n' +
+          prefix + ' voice ja ja-JP Mizuki'
+        );
+        return;
+      }
+      const lang = values[0].trim();
+      if (!POLLY_VALUES.languageCode.find(l => l.startsWith(lang))) {
+        message.reply('invalid language/言語が正しくありません: ' + lang);
+        return;
+      }
+      const langCode = values[1].trim();
+      if (!POLLY_VALUES.languageCode.find(l => l === langCode)) {
+        message.reply('invalid language code/言語コードが正しくありません: ' + langCode + ' \n' +
+                      'language code/言語コード: \n' +
+                      POLLY_VALUES.languageCode.join('\n')
+        );
+        return;
+      }
+      const voiceId = values[2].trim();
+      const voiceParam = {
+        LanguageCode: langCode,
+      };
+      Polly.describeVoices(voiceParam, (err, data) => {
+        if (!data.Voices.find(v => v.Id === voiceId)) {
+          message.reply(
+            'defined voice id is not able to speak ' + langCode + '/指定された音声IDは' + langCode + 'に対応していません \n' +
+            'voice id for ' + langCode + '/' + langCode + 'に対応する音声: \n' +
+            data.Voices.map(v => v.Id).join('\n')
+          );
+        }
+        // todo
+        if (!USERCONF[message.author.username]) {
+          USERCONF[message.author.username] = {};
+        }
+        USERCONF[message.author.username][lang] = {
+          languageCode: langCode,
+          voiceId: voiceId,
+        }
+        try {
+          fs.writeFileSync('./userconf.json', JSON.stringify(USERCONF));
+        } catch (err) {
+          console.log('could not save user conf: ' + err);
+        }
+      });
+      return;
+  } else if (message.content.startsWith(`${prefix} vid list`)) {
+    message.reply('JA: https://aws.amazon.com/jp/polly/features/#Wide_Selection_of_Voices_and_Languages');
+    message.reply('EN: https://aws.amazon.com/polly/features/#Wide_Selection_of_Voices_and_Languages');
+    return;
+  } else if (message.content.startsWith(`${prefix} la list`)) {
+    message.reply('JA: https://docs.aws.amazon.com/ja_jp/polly/latest/dg/SupportedLanguage.html');
+    message.reply('EN: https://docs.aws.amazon.com/polly/latest/dg/SupportedLanguage.html');
     return;
   }
 
-  if (langCode === 'en') {
-    langCode = 'en-GB';   // you know why.
-  } else {
-    langCode = awsLanguageCode.find(l => l.startsWith(langCode));
-    if (!langCode) {
-      message.reply('Failed to find language code/言語コードの検知に失敗しました');
-      return;
-    }
+  const streaming = streamMapping.get(channelName);
+  if (!streaming) return; // not connected.
+
+  let text = message.content;
+  if (isKusa(text)) {
+    text = text.replace('w+', 'くさ').replace('ｗ+', 'くさ');
   }
-  
+  text = text.replace(/<@!.+?>/, ''); // remove user name
+  if (text === '') {
+    return;   // TODO
+  }
+  text = text.replace(/<:.+?>/, ''); // remove custom emoji
+  if (text === '') {
+    return;   // TODO
+  }
+
+  let languageCode;
+  let audioConf;
+  try {
+    const params = {
+      TextList: [
+        text,
+      ]
+    };
+    const batchDetectDominantLanguageCommand = new BatchDetectDominantLanguageCommand(
+      params
+    );
+
+    const data = await comprehend.send(batchDetectDominantLanguageCommand);
+    // use the highest possible language.
+    const langCode = data.ResultList[0].Languages[0].LanguageCode;
+    console.log(langCode + ': ' + JSON.stringify(data));
+
+    audioConf = USERCONF[message.author.username][langCode];
+    if (audioConf) {
+      languageCode = audioConf.languageCode;
+    } else if (langCode === 'en') {
+      languageCode = 'en-GB';   // you know why.
+    } else {
+      languageCode = POLLY_VALUES.languageCode.find(l => l.startsWith(langCode));
+      if (!languageCode) {
+        message.reply('Failed to detect language/言語の検知に失敗しました [' + langCode + '] - using default : ja-JP');
+        languageCode = 'ja-JP'
+      }
+    }
+
+  } catch (error) {
+    const metadata = error.$metadata;
+    console.log(`requestId: ${metadata.requestId} cfId: ${metadata.cfId} extendedRequestId: ${metadata.extendedRequestId}`);
+    /*
+  The keys within exceptions are also parsed. You can access them by specifying exception names:
+      if(error.name === 'SomeServiceException') {
+          const value = error.specialKeyInException;
+      }
+  */
+  }
+
   const voiceParam = {
-    LanguageCode: langCode,
+    LanguageCode: languageCode,
   };
   Polly.describeVoices(voiceParam, (err, data) => {
     if (err) {
       console.log(err);
     } else if (data) {
-      voiceId = data.Voices.find(v => v.Gender === 'Female').Id;
+      let voice;
+      if (audioConf) {
+        voice = data.Voices.find(vid => vid.Id === audioConf.voiceId);
+      }
+      if (!voice) {
+        voice = data.Voices.find(v => v.Gender === 'Female');    // female by default
+      }
       const param = {
-        'Text': message.content,
+        'Text': text,
         'OutputFormat': 'mp3',
-        'VoiceId': voiceId,
-        'LanguageCode': langCode,
+        'VoiceId': voice.Id,
+        'LanguageCode': languageCode,
+        //        'Engine': voice.SupportedEngines,
       }
       getAndPlayTTS(param, message, streaming);
     }
   })
 
 });
+
+
+function isKusa(text) {
+  return text.match('w+') || text.match('ｗ+');
+}
 
 const getAndPlayTTS = (param, message, streaming) => {
   Polly.synthesizeSpeech(param, (err, data) => {
@@ -151,34 +282,38 @@ const getAndPlayTTS = (param, message, streaming) => {
       if (data.AudioStream instanceof Buffer) {
         const filename = './ttsdata/' + message.author.id + '.mp3';
         fs.writeFileSync(filename, data.AudioStream)
-        const dispatcher = streaming.vc.play(filename, {
-          volume: 0.2,
-        })
-        .on('finish', () => {
-          console.log('play done');
-          fs.unlink(filename, () => { });
-          dispatcher.destroy();
-          streaming.timestamp = Date.now();
-        })
+
+        // qeuing
+        streaming.queue.push(filename);
+        if (!streaming.playing) {
+          streaming.playing = true;
+          play(streaming.queue[0], streaming)
+        }
       }
     }
   })
 }
 
-// disconnect after 10 mins without time
+const play = (filename, streaming) => {
+  if (!filename) {
+    streaming.playing = false;
+    return;
+  }
+  const dispatcher = streaming.vc.play(filename, {
+    volume: 0.6,
+  })
+  .on('finish', () => {
+    console.log('play done');
+    fs.unlink(filename, () => { });
+    dispatcher.destroy();
+    streaming.timestamp = Date.now();
 
-if (fs.existsSync(AUTH_FILE)) {
-  console.log('auth: using auth file.');
-  var auth = require(AUTH_FILE);
-  token = auth.token;
-  auth_key = auth.auth_key;
-  prefix = auth.prefix;
-  access_key_id = auth.access_key_id;
-  secret_access_key = auth.secret_access_key;
-} else {
-  console.log('auth: not found.');
-  process.exit(1);
+    streaming.queue.shift();
+    play(streaming.queue[0], streaming);
+  });
 }
+
+// disconnect after 10 mins without time
 
 if (process.env.KEEP_ALIVE_ENDPOINT) {
   require('../heartbeat');
@@ -186,11 +321,19 @@ if (process.env.KEEP_ALIVE_ENDPOINT) {
 
 const Polly = new aws.Polly({
   signatureVersion: 'v4',
-  region: 'us-east-1',
+  region: 'eu-central-1',
   credentials: {
-    accessKeyId: access_key_id,
-    secretAccessKey: secret_access_key,
+    accessKeyId: CONFIG.access_key_id,
+    secretAccessKey: CONFIG.secret_access_key,
+  }
+});
+const comprehend = new ComprehendClient({
+  signatureVersion: 'v4',
+  region: 'eu-central-1',
+  credentials: {
+    accessKeyId: CONFIG.access_key_id,
+    secretAccessKey: CONFIG.secret_access_key,
   }
 });
 
-client.login(token);
+client.login(CONFIG.token);
